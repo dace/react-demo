fb1ca8ccc3c3f52678d3c11d5aa1e230d7935371
Dace Willmott <willmott@Brooklyns-iMac-4.local>
2015-07-30 17:16:52 -0400

    Through Lesson 8

--- app/index.js
+++ app/index.js
@@ -1,1 +1,19 @@
-index.js
+var React = require('react');
+
+var Badge = React.createClass({
+  render: function(){
+    return (
+        <button className="btn btn-primary" type="button">
+          {this.props.title} <span className="badge">{this.props.number}</span>
+        </button>
+      );
+  }
+});
+
+var options = {
+  title: 'Inbox',
+  number: 32
+};
+
+var element = React.createElement(Badge, options);
+React.render(element, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 var path = require("path"); 
 var webpack = require("webpack");
-var ExtractTextPlugin = require("extract-text-webpack-plugin"); // minifies css
+var ExtractTextPlugin = require("extract-text-webpack-plugin"); 
 
 module.exports = {
   entry: {
-    bundle: path.join(__dirname, "app", "index.js") // removed "/"s because of how Windows handles them
+    bundle: path.join(__dirname, "app", "index.js")
   },
   output: {
-    path: path.join(__dirname, "public", "build"), // removed "/"s because of how Windows handles them
+    path: path.join(__dirname, "public", "build"),
     filename: "[name].js"
   },
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: "babel?stage=0&optional=runtime" //  what does runtime do?
+      loader: "babel?stage=0&optional=runtime"
     }, {
       test: /\.(css|styl)$/, 
       exclude: /node_modules/,
-      loader: ExtractTextPlugin.extract("style", "css!stylus") // unclear as to what this does exactly
+      loader: ExtractTextPlugin.extract("style", "css!stylus")
     }]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin("/stylesheets/[name].css") // saves minified css to this directory/file
+    new ExtractTextPlugin("/stylesheets/[name].css")
   ],
   stylus: {
     use: [require("nib")(), require("rupture")()]
